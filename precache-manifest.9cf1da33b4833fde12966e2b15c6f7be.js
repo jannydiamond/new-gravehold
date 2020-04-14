efde42fbe7abded4577541ac24727bb976969ec1
Janett Hickel <janett.hickel@googlemail.com>
2020-04-14 22:16:06 +0200

    Updates

---
+++ precache-manifest.9cf1da33b4833fde12966e2b15c6f7be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d114bfee48f33057207252730e5622d9",
+    "url": "/aer-custom-expedition-editor/index.html"
+  },
+  {
+    "revision": "0a184f7d4d6de14ca9c5",
+    "url": "/aer-custom-expedition-editor/static/js/2.f3597ddc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aer-custom-expedition-editor/static/js/2.f3597ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b1719be319e07cbff3",
+    "url": "/aer-custom-expedition-editor/static/js/main.a00a9ee6.chunk.js"
+  },
+  {
+    "revision": "37d2a7bbe05d6f7ed386",
+    "url": "/aer-custom-expedition-editor/static/js/runtime-main.79d09fcd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aer-custom-expedition-editor/precache-manifest.2450754adfca0736221410219923067b.js"
+  "/aer-custom-expedition-editor/precache-manifest.9cf1da33b4833fde12966e2b15c6f7be.js"
 );
 
 self.addEventListener('message', (event) => {
