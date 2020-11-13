ec0ecb681706701ebc9ee2121dbc8d544ba80458
Janett Hickel <janett.hickel@googlemail.com>
2020-11-13 15:42:34 +0100

    TASK: Add npm version to app

--- src/components/App/MainApp.tsx
+++ src/components/App/MainApp.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import H1 from 'components/atoms/H1'
+import H2 from 'components/atoms/H2'
+import P from 'components/atoms/P'
+import Link from 'components/atoms/Link'
 import Configurator from 'components/molecules/Configurator'
 import Wrapper from './__styled__/Wrapper'
 
@@ -8,6 +11,14 @@ const MainApp = () => {
   return (
     <Wrapper>
       <H1>AER: Custom Expedition Editor</H1>
+      <H2>Version: {process.env.REACT_APP_VERSION}</H2>
+      <P>
+        This is an alpha version of the Custom Expedition Editor for the{' '}
+        <Link href="https://aeons-end-randomizer.de" target="_blank">
+          Aeons End Randomizer
+        </Link>
+        .
+      </P>
       <Configurator />
       <div id="modal-root" />
     </Wrapper>
---
+++ src/components/atoms/P.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components/macro'
+
+const P = styled('p')``
+
+export default P
